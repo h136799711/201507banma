6f5d6720e3dca9533af789f7fbda2ee627b8fe66
h136799711 <hebiduhebi@126.com>
2015-07-30 14:29:22 +0800

    INIT

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -22,10 +22,10 @@ return array(
 	// 程序版本
 	// DONE:移到数据库中
 	// 显示运行时间
-	'SHOW_RUN_TIME'=>true,
+//	'SHOW_RUN_TIME'=>true,
 //	'SHOW_ADV_TIME'=>true,
 	// 显示数据库操作次数
-	'SHOW_DB_TIMES'=>true,
+//	'SHOW_DB_TIMES'=>true,
 	// 显示操作缓存次数
 //	'SHOW_CACHE_TIMES'=>true,
 	// 显示使用内存
