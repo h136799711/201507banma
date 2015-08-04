046a23aadb29009e1f3347159084f16848f0ef27
hebidu <hebidu@Gooraye-006>
2015-08-04 18:47:12 +0800

    update

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -1,36 +1,11 @@
 <?php
-// .-----------------------------------------------------------------------------------
-// | WE TRY THE BEST WAY 杭州博也网络科技有限公司
-// |-----------------------------------------------------------------------------------
-// | Author: 青 <99701759@qq.com>
-// | Copyright (c) 2013-2016, http://www.itboye.com. All Rights Reserved.
-// |-----------------------------------------------------------------------------------
-
 namespace Home\Controller;
 use Think\Controller;
 class IndexController extends Controller {
     public function index(){
-        $this->display();
+        $this->show("首页");
     }
-	
-	/*
-	 * 注册
-	 * TODO:短信注册
-	 * */
-	public function register(){
-		if(IS_GET){
-			$this->display();
-		}
-	}
-	/*
-	 * 登录
-	 * 
-	 * */
-	public function login(){
-		if(IS_GET){
-			$this->display();
-		}
-	}
+
     public function qrcode(){
 
         vendor("phpqrcode.phpqrcode");
